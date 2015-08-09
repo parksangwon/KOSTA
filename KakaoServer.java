c1d3015bd82c2beeaed3d35f84a94696da5dacf9
young20111 <young20111@naver.com>
2015-08-09 15:24:09 +0900

    성권-서버

--- KakaoServer.java
+++ KakaoServer.java
@@ -11,44 +11,73 @@ public class KakaoServer extends UnicastRemoteObject implements KakaoServerInter
 
 	public KakaoServer() throws RemoteException{} // 디폴트 생성자
 
-	public void setClient(KakaoClientInterface client, String name, String talkName) throws RemoteException{
-		User user = new User(client, name, talkName);
+	public void setClient(KakaoClientInterface client,  String userId, String name, String talkName) throws RemoteException{
+		User user = new User(client, userId, name, talkName);
 		users.add(user); // 접속한 유저 저장
 		System.out.println(name + "님이 접속했습니다. 현재 " + users.size() + "명이 접속해있습니다.");
 	}
+
 	public ArrayList<User> getClient() throws RemoteException{
 		return users; // 접속한 유저 반환
 	}
 
-	public void removeClient(int index) throws RemoteException{
-		User user = users.get(index); // 이 이후에 있는 사람들은 모두 인덱스 하나씩 감소
-		users.remove(index);
-		System.out.println(user.getName() + "님이 접속을 종료했습니다. 현재 " + users.size() + "명이 접속해있습니다.");
-	}
+	// 접속 종료 시 유저 삭제
+	public void removeClient(String userId) throws RemoteException{
 
-	public void setRoom(Room room) throws RemoteException{
-		rooms.add(room); // 방 정보를 유지함
-	}
+		User user = null;
 
-	public void setMessage(Room room, String msg) throws Exception{
-
-		//Thread로 구현??
-		for(int i = 0 ; i < room.getIndex().size() ; i++){ // 방에 있는 사람 수
-
-			int UserIndex = room.getIndex().get(i); // 인덱스를 가져와서 유저를 찾기위해
-			User user = users.get(UserIndex); // 유저에게 메시지 전달
-			try
-			{
-				KakaoClientInterface kci = user.getClient();
-				kci.setMessage(msg);
-			}
-			catch (ConnectException ce)
-			{
-				System.out.println("나간사람 발생");
-				room.getIndex().remove(i);
-				i--;
+		for(int i = 0 ; i < users.size() ; i++){
+			if(userId.equals(users.get(i).getName())){
+				user = users.get(i);
 			}
 		}
+		if(user != null){
+			users.remove(user);
+			System.out.println(user.getName() + "님이 접속을 종료했습니다. 현재 " + users.size() + "명이 접속해있습니다.");
+		}
+
+	}
+
+	// 채팅방 추가 시 방 정보 저장
+	public void setRoom(String roomId, String roomName, ArrayList<String> userIdList) throws RemoteException{
+		rooms.add(new Room(roomId, roomName, userIdList)); // 방 정보를 유지함
+	}
+
+	// 특정 방에 메시지 보내기
+	public void setMessage(String roomId, String msg) throws Exception{
+		
+		Room room = null;
+
+		for(int i = 0 ; i < rooms.size() ; i++){// 방 식별
+			if(roomId.equals(rooms.get(i).getRoomId())){
+				room = rooms.get(i);
+			}
+		}
+		if(room != null){// 방에 있는 사람 수
+			for(int i = 0 ; i < room.getUserIdList().size() ; i++){
+
+				String UserId = room.getUserIdList().get(i); // id로 유저를 찾음
+				User user = null;
+
+				if(UserId.equals(users.get(i).getUserId())){
+
+					user = users.get(i);
+
+					try
+					{
+						KakaoClientInterface kci = user.getClient();
+						kci.setMessage(msg);
+					}
+					catch (ConnectException ce)
+					{
+						System.out.println("나간사람 발생");
+						//*********************************추가
+						//room.getIndex().remove(i);
+						//i--;
+					}
+				} // if
+			} // for
+		} // if
 	}
 	
 	public static void main(String[] args) throws Exception{
--- KakaoServerInterface.java
+++ KakaoServerInterface.java
@@ -3,9 +3,9 @@ import java.util.*;
 
 public interface KakaoServerInterface extends Remote
 {
-	public void setClient(KakaoClientInterface client, String name, String talkName) throws RemoteException;
+	public void setClient(KakaoClientInterface client,  String userId, String name, String talkName) throws RemoteException;
 	public ArrayList<User> getClient() throws RemoteException;
-	public void removeClient(int index) throws RemoteException;
-	public void setRoom(Room room) throws RemoteException;
-	public void setMessage(Room room, String msg) throws Exception;
+	public void removeClient(String userId) throws RemoteException;
+	public void setRoom(String roomId, String roomName, ArrayList<String> userList) throws RemoteException;
+	public void setMessage(String roomId, String msg) throws Exception;
 }
