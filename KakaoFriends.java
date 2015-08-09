3ae9a1b1386dad888f30ce54a2d2c697bdcc8738
poyal <ttt4504@gmail.com>
2015-08-09 14:32:58 +0900

    [경수]
    KakaoFriends GUI개편 및 x버튼 _버튼 추가 KakaoTalk타이틀 추가

--- KakaoFriends.java
+++ KakaoFriends.java
@@ -6,9 +6,9 @@ import java.util.*;
 public class KakaoFriends extends JPanel
 {
 	//panel4
-	JPanel controlPanel, viewPanel;
-	JButton listButton;
-	JLabel title;
+	JPanel controlPanel;
+	JButton listButton/*, minimize*/, exit;
+	JLabel title, titleTalk, titleKakao;
 	DefaultListModel listModel;
 	JList friendList;
 	JScrollPane scrollPane;
@@ -16,16 +16,56 @@ public class KakaoFriends extends JPanel
 	{
 		//card4
 		controlPanel = new JPanel();
-		viewPanel = new JPanel();
+		titleKakao = new JLabel("Kakao");
+		titleTalk = new JLabel("Talk");
 		listButton = new JButton("¸ñ·Ï"); 
-		title = new JLabel("         Ä£±¸¸ñ·Ï         ");
+		title = new JLabel("Ä£±¸¸ñ·Ï");
 		listModel = new DefaultListModel();
 		friendList = new JList(listModel);
 		scrollPane = new JScrollPane(friendList);
+		//minimize = new JButton("_");
+		exit = new JButton("X");;
 
-		setLayout(new BorderLayout());
+		setLayout(null);
+		controlPanel.setLayout(null);
 		controlPanel.add(title);
+		controlPanel.add(titleKakao);
+		controlPanel.add(titleTalk);
 		controlPanel.add(listButton);
+		//controlPanel.add(minimize);
+		controlPanel.add(exit); 
+
+		controlPanel.setBounds(0, 0, 350, 100);
+		controlPanel.setBackground(new Color(69,44,42));
+		
+		titleKakao.setBounds(10 ,3 ,70, 30);
+		titleKakao.setForeground(new Color(255,255,255));
+		titleKakao.setFont(new Font("¸¼Àº°íµñ",Font.PLAIN,20));
+		titleTalk.setBounds(65 ,3 ,70, 30);
+		titleTalk.setForeground(new Color(255,255,255));
+		titleTalk.setFont(new Font("¸¼Àº°íµñ",Font.BOLD,20));
+
+		title.setBounds(150, 40,	 65, 30);
+		title.setForeground(new Color(255,255,255));
+		title.setFont(new Font("¸¼Àº°íµñ",Font.BOLD,15));
+		listButton.setBounds(250, 40,	 70, 30);
+		scrollPane.setBounds(0, 100,	 350, 500);
+
+		/*
+		minimize.setBackground(new Color(255,235,51));
+		minimize.setFont(new Font("¸¼Àº°íµñ",Font.BOLD,15));
+		minimize.setBorder(null);
+		minimize.setFocusPainted(false);
+		minimize.addActionListener(new KakaoSizeEvent(this));
+		*/
+
+		exit.setBackground(new Color(69,44,42));
+		exit.setFont(new Font("¸¼Àº°íµñ",Font.BOLD,12));
+		exit.setForeground(new Color(255,255,255));
+		exit.setBorder(null);
+		exit.setFocusPainted(false);
+		exit.setBounds(330,5, 20, 20);
+		//exit.addActionListener(new )
 
 		listModel.addElement("f1");	 listModel.addElement("g2");
 		listModel.addElement("f2");	 listModel.addElement("g3 g3");
@@ -34,8 +74,8 @@ public class KakaoFriends extends JPanel
 		
 		friendList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		
-		add("North",controlPanel);
-		add("Center",scrollPane);
+		add(controlPanel);
+		add(scrollPane);
 	}
 
 }
