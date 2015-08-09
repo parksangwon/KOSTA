9c8184002832da616f4627f2e39168f316fae5ca
sangwon <ourtree4@gmail.com>
2015-08-09 15:39:10 +0900

    [상원] 채팅리스트에서 입장할때 할 작업
    [상원] 채팅리스트에서 입장할때 할 작업

--- KakaoListEvent.java
+++ KakaoListEvent.java
@@ -5,9 +5,11 @@ import java.util.*;
 public class KakaoListEvent implements ActionListener
 {
 	private KakaoTalk cr;
+	private KakaoList kakaoList;
 	public KakaoListEvent(KakaoTalk cr)
 	{
 		this.cr = cr;
+		this.kakaoList = cr.chatListView;
 	}
 	public void actionPerformed(ActionEvent e)
 	{		
@@ -16,7 +18,20 @@ public class KakaoListEvent implements ActionListener
 		if (jb.equals("입장"))
 		{
 			// 자신의 방목록중에 하나 선택해서 들가야함
-			cr.card.show(cr.slide,"kakaoChat");
+			// 선택된 채팅방을 추출
+			int index = kakaoList.chatList.getSelectedIndex();
+			String selectedItem = (String)kakaoList.listModel.getElementAt(index);
+			//  추출된 채팅방이 null이면 리턴
+			if ( selectedItem == null ) {
+				return;
+			}
+			String[] strArr = selectedItem.split("-");
+			String roomId = strArr[0];
+			//String roomName = strArr[1];
+			//  null이 아니면 카드를 보여줘야하는데.
+			cr.card.show(cr.slide,"kakaoSlide");
+			KakaoSlide kakaoSlide = cr.chatSlide;
+			kakaoSlide.chatCard.show(kakaoSlide.slide, roomId);
 		}
 
 		if (jb.equals("친구목록"))
